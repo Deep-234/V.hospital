8413f7896daafd88d60ed28e52e68b2cced2ac02
Deep Chaudhary <chaudharydeep118@gmail.com>
2021-09-20 15:39:11 +0530

    make it better

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports ={
+    MONGOURI:"mongodb+srv://deep:Mongodb@cluster0.tii2u.mongodb.net/test",
+    JWT_SECRET:"erechutdwsfg",
+    SENDGRID_API:"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"
+}
+
+
+// if(process.env.NODE_ENV==='production'){
+//     module.exports = require('./prod')
+// }else{
+//     module.exports = require('./dev')
+// }
