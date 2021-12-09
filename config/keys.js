bb921e536181580bf3af945e9cafaa16cde185b9
Deep Chaudhary <chaudharydeep118@gmail.com>
2021-12-09 13:29:08 +0530

    first commit

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
-module.exports ={
-    MONGOURI:"mongodb+srv://deep:Mongodb@cluster0.tii2u.mongodb.net/test",
-    JWT_SECRET:"erechutdwsfg",
-    SENDGRID_API:"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"
-}
-
+module.exports = {
+  MONGOURI: "mongodb+srv://deep:Mongodb@cluster0.tii2u.mongodb.net/test1234",
+  JWT_SECRET: "erechutdwsfg",
+  SENDGRID_API:
+    "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",
+};
 
 // if(process.env.NODE_ENV==='production'){
 //     module.exports = require('./prod')
--- models/post.js
+++ models/post.js
@@ -1,27 +1,49 @@
-const mongoose = require('mongoose')
-const {ObjectId} = mongoose.Schema.Types
-const postSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const postSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    body:{
-        type:String,
-        required:true
+    qualification: {
+      type: String,
+      required: true,
     },
-    photo:{
-        type:String,
-        required:true
+    address: {
+      type: String,
+      required: true,
     },
-    likes:[{type:ObjectId,ref:"User"}],
-    comments:[{
-        text:String,
-        postedBy:{type:ObjectId,ref:"User"}
-    }],
-    postedBy:{
-       type:ObjectId,
-       ref:"User"
-    }
-},{timestamps:true})
+    timing: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: String,
+      required: true,
+    },
 
-mongoose.model("Post",postSchema)
+    weblink: {
+      type: String,
+      required: true,
+    },
+    pic: {
+      type: String,
+      required: true,
+    },
+    likes: [{ type: ObjectId, ref: "User" }],
+    comments: [
+      {
+        text: String,
+        postedBy: { type: ObjectId, ref: "User" },
+      },
+    ],
+    postedBy: {
+      type: ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+mongoose.model("Post", postSchema);
